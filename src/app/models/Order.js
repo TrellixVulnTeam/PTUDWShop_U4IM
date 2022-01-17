bfd4eb074267b4f68c2c9a1b10a328bd8bae1caf
xol48 <94368164+xol48@users.noreply.github.com>
2022-01-17 10:49:22 +0700

    update order controller, orderdetail controller

--- src/app/models/Order.js
+++ src/app/models/Order.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const Order = new Schema({
-     _iduser: { type: String,require:true},
+    _iduser: { type: String,require:true},
     _status:{ type: String,require:true},
     _book:{type:Date},
-    _delivery:{type:Date}
+    _delivery:{type:Date},
+    _address:{type:String},
+    _phonenumber:{type:String},
+    _note:{type:String},
+    _total:{type:String}
 })
 module.exports=mongoose.model('Order',Order);
--- src/app/models/Orderdetail.js
+++ src/app/models/Orderdetail.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const Orderdetail = new Schema({
      _idorder: { type: String,require:true},
-     _idproduct: { type: String,require:true},
-     _amount:{ type: Number, default: 0 }
+    _idproduct: { type: String,require:true},
+    _nameproduct:{type:String},
+    _amount:{ type: Number, default: 0 },
+    _total:{type:Number}
 })
 module.exports=mongoose.model('Orderdetail',Orderdetail);
