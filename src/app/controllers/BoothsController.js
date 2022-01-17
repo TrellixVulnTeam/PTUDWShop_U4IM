aada378fd385c1ae0a0f1d814c2465e7f75f2819
xol48 <94368164+xol48@users.noreply.github.com>
2022-01-17 12:28:23 +0700

    link booth from main.hbs

--- src/app/controllers/BoothsController.js
+++ src/app/controllers/BoothsController.js
@@ -2,30 +2,44 @@ const Product=require('../models/Product')
 const { mongooseToObject } = require('../../util/mongoose');
 class BoothsController {
     show(req,res){
+        if(req.session.admin){
         var procedure=req.query.p;
         Product.find({_procedure:procedure})
         .lean()
         .then((products)=>{
-            res.render('booths/show',{products})
+            res.render('booths/show',{products,
+            admin:req.session.admin,})
         })
     }
+    else
+    {
+        res.redirect("/accounts/login");
+    }
+    }
     edit(req,res){
+        if(req.session.admin)
+        {
         Product.findById(req.params.id)
         .then((product) =>{
             if(product._booth==true)
             {
                 var display=1;
             res.render('booths/update', {
-                product: mongooseToObject(product),display
+                product: mongooseToObject(product),display,
+                admin:req.session.admin,
             })
         }
         else{
             var notdisplay=1;
             res.render('booths/update', {
+                admin:req.session.admin,
                 product: mongooseToObject(product),notdisplay
             })
         }
-    })
+    })}
+    else{
+        res.redirect("/accounts/login");
+    }
     }
     save(req,res){
         Product.findById(req.params.id)
